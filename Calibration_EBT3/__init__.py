4690f32c956a2714940ac065935e7a2077463759
Eleonora Amico <eleonora.c.amico@gmail.com>
2025-02-14 19:11:33 +0100

    Added __init__.py to initialize the package

--- Calibration_EBT3/__init__.py
+++ Calibration_EBT3/__init__.py
@@ -2,6 +2,8 @@
 """
 Created on Thu Feb 13 21:54:51 2025
 
-@author: Ele_p
+@author: Eleonora Cristina Amico
 """
-
+from .Calibration_EBT3 import CurveFitter, ProcessingMode
+from .plot_function import FitPlotter, PlotType
+__all__ = ['CurveFitter', 'FitPlotter', 'ProcessingMode','PlotType' ]
