7dda29c8ccedf448f164814d4c008bccdb70c2c6
Eleonora Amico <eleonora.c.amico@gmail.com>
2025-03-12 19:23:03 +0100

    upload init for class single and multi channel calibration

--- Calibration_EBT3/__init__.py
+++ Calibration_EBT3/__init__.py
@@ -6,4 +6,7 @@ Created on Thu Feb 13 21:54:51 2025
 """
 from .Calibration_EBT3 import CurveFitter, ProcessingMode
 from .plot_function import FitPlotter, PlotType
-__all__ = ['CurveFitter', 'FitPlotter', 'ProcessingMode','PlotType' ]
+from .single_channel_calibration import SingleChannelCalibration
+from .multi_calibration import MultiChannelCalibration
+__all__ = ['CurveFitter', 'FitPlotter', 'ProcessingMode','PlotType', 
+           'SingleChannelCalibration', 'MultiChannelCalibration' ]
